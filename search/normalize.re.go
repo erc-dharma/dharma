b5da3291840341957c798a4988cd062108236840
Michaël Meyer <michaelnm.meyer@gmail.com>
2026-06-26 19:14:00 +0200

    Simplify the internal text encoding

--- search/normalize.re.go
+++ search/normalize.re.go
@@ -20,89 +20,19 @@ var (
 	_ = cases.Fold
 )
 
-const (
-	Pnul = iota
-	Pa
-	Pā
-	Pi
-	Pī
-	Pu
-	Pū
-	Pṛ
-	Pṝ
-	Pḷ
-	Pḹ
-	Pe
-	Pai
-	Po
-	Pau
-	Pṃ
-	Pḥ
-	Pk
-	Pkh
-	Pg
-	Pgh
-	Pṅ
-	Pc
-	Pch
-	Pj
-	Pjh
-	Pñ
-	Pṭ
-	Pṭh
-	Pḍ
-	Pḍh
-	Pṇ
-	Pt
-	Pth
-	Pd
-	Pdh
-	Pn
-	Pp
-	Pph
-	Pb
-	Pbh
-	Pm
-	Py
-	Pr
-	Pl
-	Pv
-	Pś
-	Pṣ
-	Ps
-	Ph
-	Pschwa
-	Plongschwa
-	P0
-	P1
-	P2
-	P3
-	P4
-	P5
-	P6
-	P7
-	P8
-	P9
-	Pother
-)
-
-var phonemeLookup = []string{
-	"!",
-	"a", "ā", "i", "ī", "u", "ū",
-	"ṛ", "ṝ", "ḷ", "ḹ",
-	"e", "ai", "o", "au",
-	"ṃ", "ḥ",
-	"k", "kh", "g", "gh", "ṅ",
-	"c", "ch", "j", "jh", "ñ",
-	"ṭ", "ṭh", "ḍ", "ḍh", "ṇ",
-	"t", "th", "d", "dh", "n",
-	"p", "ph", "b", "bh", "m",
-	"y", "r", "l", "v",
-	"ś", "ṣ", "s", "h",
-	"ə", "ə̄",
-	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-	"?",
-}
+const Pai = 0xE002
+const Pau = 0xE003
+const Pkh = 0xE004
+const Pgh = 0xE005
+const Pch = 0xE006
+const Pjh = 0xE007
+const P_th = 0xE008
+const P_dh = 0xE009
+const Pth = 0xE00A
+const Pdh = 0xE00B
+const Pph = 0xE00C
+const Pbh = 0xE00D
+const Plongschwa = 0xE00E
 
 // peekByte safely reads a byte from a string to prevent out-of-bounds panics in re2c.
 // It returns 0 when the cursor reaches or exceeds the string length.
@@ -113,20 +43,9 @@ func peekByte(s string, i int) byte {
 	return 0
 }
 
-// makePrintable converts the internal binary representation back to readable characters.
-// It maps sequence codes to their IAST equivalents for debugging purposes.
-func makePrintable(s string) string {
-	var buf strings.Builder
-	for i := 0; i < len(s); i++ {
-		c := int(s[i])
-		buf.WriteString(phonemeLookup[c])
-	}
-	return buf.String()
-}
-
 // lexPrefix uses a finite state machine to identify the next semantic sequence.
 // It returns the byte length of the matched sequence, its replacement, and an elision flag.
-func lexPrefix(text string) (int, int, bool) {
+func lexPrefix(text string) (int, rune, bool) {
 	cursor, marker := 0, 0
 	_ = cursor
 	_ = marker
@@ -139,7 +58,7 @@ func lexPrefix(text string) (int, int, bool) {
 	re2c:define:YYSKIP = "cursor++";
 	re2c:define:YYBACKUP = "marker = cursor";
 	re2c:define:YYRESTORE = "cursor = marker";
-	* { return 1, Pnul, true }
+	* { return 1, 0, true }
 	// In Tamil, the character "'" represents an elided "u" when it appears
 	// at the end of a word (viz. before a non-alphanumeric char or the
 	// empty string), as in:
@@ -156,78 +75,68 @@ func lexPrefix(text string) (int, int, bool) {
 	// character into an "a".
 	// The sequences "'!" and "’!" always represent the avagraha, e.g.
 	// ’!pi = 'pi = api
-	"a" | "A" | "ă" | "Ă" | "'" | "’" | "'!" | "’!" { return cursor, Pa, false }
-	"ā" | "Ā" { return cursor, Pā, false }
-	"i" | "I" | "ĭ" | "Ĭ" { return cursor, Pi, false }
-	"ī" | "Ī" { return cursor, Pī, false }
-	"u" | "U" | "ŭ" | "Ŭ" { return cursor, Pu, false }
-	"ū" | "Ū" { return cursor, Pū, false }
-	"ṛ" | "Ṛ" | "r̥" | "R̥" { return cursor, Pṛ, false }
-	"ṝ" | "Ṝ" | "r̥̄" | "R̥̄" { return cursor, Pṝ, false }
-	"ḷ" | "Ḷ" | "l̥" | "L̥"{ return cursor, Pḷ, false }
-	"ḹ" | "Ḹ" | "l̥̄" | "L̥̄" { return cursor, Pḹ, false }
-	"e" | "E" | "ĕ" | "Ĕ" | "ē" | "Ē" { return cursor, Pe, false }
+	"a" | "A" | "ă" | "Ă" | "'" | "’" | "'!" | "’!" { return cursor, 'a', false }
+	"ā" | "Ā" { return cursor, 'ā', false }
+	"i" | "I" | "ĭ" | "Ĭ" { return cursor, 'i', false }
+	"ī" | "Ī" { return cursor, 'ī', false }
+	"u" | "U" | "ŭ" | "Ŭ" { return cursor, 'u', false }
+	"ū" | "Ū" { return cursor, 'ū', false }
+	"ṛ" | "Ṛ" | "r̥" | "R̥" { return cursor, 'ṛ', false }
+	"ṝ" | "Ṝ" | "r̥̄" | "R̥̄" { return cursor, 'ṝ', false }
+	"ḷ" | "Ḷ" | "l̥" | "L̥"{ return cursor, 'ḷ', false }
+	"ḹ" | "Ḹ" | "l̥̄" | "L̥̄" { return cursor, 'ḹ', false }
+	"e" | "E" | "ĕ" | "Ĕ" | "ē" | "Ē" { return cursor, 'e', false }
 	"ai" | "Ai" | "AI" | "aI" { return cursor, Pai, false }
-	"o" | "O" | "ŏ" | "Ŏ" | "ō" | "Ō" { return cursor, Po, false }
+	"o" | "O" | "ŏ" | "Ŏ" | "ō" | "Ō" { return cursor, 'o', false }
 	"au" | "Au" | "AU" | "aU" { return cursor, Pau, false }
 	// Anusvara and anunāsika, Cam anusvāra-candra, all treated as the anusvara.
-	"ṁ" | "Ṁ" | "ṃ" | "Ṃ" | "m̐" | "M̐" | "m̃" | "M̃" { return cursor, Pṃ, false }
+	"ṁ" | "Ṁ" | "ṃ" | "Ṃ" | "m̐" | "M̐" | "m̃" | "M̃" { return cursor, 'ṃ', false }
 	// Upadhmānīya and jihvāmūlīya. Just fold them to a visarga.
-	"ḥ" | "Ḥ" | "ḫ" | "Ḫ" | "ẖ" | "H̱" { return cursor, Pḥ, false }
-	"k" | "K" { return cursor, Pk, false }
+	"ḥ" | "Ḥ" | "ḫ" | "Ḫ" | "ẖ" | "H̱" { return cursor, 'ḥ', false }
+	"k" | "K" { return cursor, 'k', false }
 	"kh" | "Kh" | "KH" | "kH" { return cursor, Pkh, false }
-	"g" | "G" { return cursor, Pg, false }
+	"g" | "G" { return cursor, 'g', false }
 	"gh" | "Gh" | "GH" | "gH" { return cursor, Pgh, false }
-	"ṅ" | "Ṅ" { return cursor, Pṅ, false }
-	"c" | "C" { return cursor, Pc, false }
+	"ṅ" | "Ṅ" { return cursor, 'ṅ', false }
+	"c" | "C" { return cursor, 'c', false }
 	"ch" | "Ch" | "CH" | "cH" { return cursor, Pch, false }
-	"j" | "J" { return cursor, Pj, false }
+	"j" | "J" { return cursor, 'j', false }
 	"jh" | "Jh" | "JH" | "jH" { return cursor, Pjh, false }
-	"ñ" | "Ñ" { return cursor, Pñ, false }
-	"ṭ" | "Ṭ" { return cursor, Pṭ, false }
-	"ṭh" | "Ṭh" | "ṬH" | "ṭH" { return cursor, Pṭh, false }
-	"ḍ" | "Ḍ" { return cursor, Pḍ, false }
-	"ḍh" | "Ḍh" | "ḌH" | "ḍH" { return cursor, Pḍh, false }
-	"ṇ" | "Ṇ" { return cursor, Pṇ, false }
-	"t" | "T" { return cursor, Pt, false }
+	"ñ" | "Ñ" { return cursor, 'ñ', false }
+	"ṭ" | "Ṭ" { return cursor, 'ṭ', false }
+	"ṭh" | "Ṭh" | "ṬH" | "ṭH" { return cursor, P_th, false }
+	"ḍ" | "Ḍ" { return cursor, 'ḍ', false }
+	"ḍh" | "Ḍh" | "ḌH" | "ḍH" { return cursor, P_dh, false }
+	"ṇ" | "Ṇ" { return cursor, 'ṇ', false }
+	"t" | "T" { return cursor, 't', false }
 	"th" | "Th" | "TH" | "tH" { return cursor, Pth, false }
-	"d" | "D" { return cursor, Pd, false }
+	"d" | "D" { return cursor, 'd', false }
 	"dh" | "Dh" | "DH" | "dH" { return cursor, Pdh, false }
-	"n" | "N" { return cursor, Pn, false }
-	"p" | "P" { return cursor, Pp, false }
+	"n" | "N" { return cursor, 'n', false }
+	"p" | "P" { return cursor, 'p', false }
 	"ph" | "Ph" | "PH" | "pH" { return cursor, Pph, false }
-	"b" | "B" { return cursor, Pb, false }
+	"b" | "B" { return cursor, 'b', false }
 	"bh" | "Bh" | "BH" | "bH" { return cursor, Pbh, false }
-	"m" | "M" { return cursor, Pm, false }
-	"y" | "Y" { return cursor, Py, false }
-	"r" | "R" { return cursor, Pr, false }
-	"l" | "L" { return cursor, Pl, false }
-	"v" | "V" { return cursor, Pv, false }
-	"ś" | "Ś" { return cursor, Pś, false }
-	"ṣ" | "Ṣ" { return cursor, Pṣ, false }
-	"s" | "S" { return cursor, Ps, false }
-	"h" | "H" { return cursor, Ph, false }
+	"m" | "M" { return cursor, 'm', false }
+	"y" | "Y" { return cursor, 'y', false }
+	"r" | "R" { return cursor, 'r', false }
+	"l" | "L" { return cursor, 'l', false }
+	"v" | "V" { return cursor, 'v', false }
+	"ś" | "Ś" { return cursor, 'ś', false }
+	"ṣ" | "Ṣ" { return cursor, 'ṣ', false }
+	"s" | "S" { return cursor, 's', false }
+	"h" | "H" { return cursor, 'h', false }
 	// Javanese/Balinese pepet. For the following, we both map
 	// LATIN SMALL LETTER SCHWA (the correct character) and CYRILLIC
 	// SMALL LETTER SCHWA (incorrect one), in both the upper- and lowercase versions.
-	"ə" | "Ə" | "ә" | "Ә" { return cursor, Pschwa, false }
+	"ə" | "Ə" | "ә" | "Ә" { return cursor, 'ə', false }
 	"ə̄" | "Ə̄" | "ә̄" | "Ә̄" { return cursor, Plongschwa, false }
-	"0" { return cursor, P0, false }
-	"1" { return cursor, P1, false }
-	"2" { return cursor, P2, false }
-	"3" { return cursor, P3, false }
-	"4" { return cursor, P4, false }
-	"5" { return cursor, P5, false }
-	"6" { return cursor, P6, false }
-	"7" { return cursor, P7, false }
-	"8" { return cursor, P8, false }
-	"9" { return cursor, P9, false }
 	[^] {
 		r, _ := utf8.DecodeRuneInString(text[:cursor])
 		if !unicode.IsLetter(r) && !unicode.IsDigit(r) {
-			return cursor, Pnul, true
+			return cursor, 0, true
 		}
-		return cursor, Pother, false
+		return cursor, r, false
 	}
 	*/
 	return 0, 0, false
@@ -235,7 +144,7 @@ func lexPrefix(text string) (int, int, bool) {
 
 // consumeToken reads the next token and aligns the cursor with grapheme boundaries.
 // It absorbs any trailing combining characters attached to the recognized sequence.
-func consumeToken(text string) (int, int, bool) {
+func consumeToken(text string) (int, rune, bool) {
 	matchLen, rep, elide := lexPrefix(text)
 	consumed := 0
 	state := -1
@@ -250,110 +159,64 @@ func consumeToken(text string) (int, int, bool) {
 
 // lexReduced applies phonological transformations on the binary representation.
 // It maps encoded sequences to a further reduced structural logic.
-func lexReduced(encoded string) (int, int) {
+func lexReduced(encoded string) (int, rune) {
 	cursor, marker := 0, 0
 	_ = cursor
 	_ = marker
 	_ = encoded
 	/*!re2c
-	re2c:flags:8 = 0;
+	re2c:flags:8 = 1;
 	re2c:yyfill:enable = 0;
 	re2c:define:YYCTYPE = byte;
 	re2c:define:YYPEEK = "peekByte(encoded, cursor)";
 	re2c:define:YYSKIP = "cursor++";
 	re2c:define:YYBACKUP = "marker = cursor";
 	re2c:define:YYRESTORE = "cursor = marker";
-	* { return 1, Pnul }
-	"\x00" { return 1, Pnul }
-	Pa = "\x01";
-	Paa = "\x02";
-	Pi = "\x03";
-	Pii = "\x04";
-	Pu = "\x05";
-	Puu = "\x06";
-	P_r = "\x07";
-	P_rr = "\x08";
-	P_l = "\x09";
-	P_ll = "\x0A";
-	Pe = "\x0B";
-	Pai = "\x0C";
-	Po = "\x0D";
-	Pau = "\x0E";
-	P_m = "\x0F";
-	P_h = "\x10";
-	Pk = "\x11";
-	Pkh = "\x12";
-	Pg = "\x13";
-	Pgh = "\x14";
-	Pf = "\x15";
-	Pc = "\x16";
-	Pch = "\x17";
-	Pj = "\x18";
-	Pjh = "\x19";
-	Ppalataln = "\x1A";
-	P_t = "\x1B";
-	P_th = "\x1C";
-	P_d = "\x1D";
-	P_dh = "\x1E";
-	P_n = "\x1F";
-	Pt = "\x20";
-	Pth = "\x21";
-	Pd = "\x22";
-	Pdh = "\x23";
-	Pn = "\x24";
-	Pp = "\x25";
-	Pph = "\x26";
-	Pb = "\x27";
-	Pbh = "\x28";
-	Pm = "\x29";
-	Py = "\x2A";
-	Pr = "\x2B";
-	Pl = "\x2C";
-	Pv = "\x2D";
-	Ppalatals = "\x2E";
-	P_s = "\x2F";
-	Ps = "\x30";
-	Ph = "\x31";
-	Pschwa = "\x32";
-	Plongschwa = "\x33";
-	P0 = "\x34";
-	P1 = "\x35";
-	P2 = "\x36";
-	P3 = "\x37";
-	P4 = "\x38";
-	P5 = "\x39";
-	P6 = "\x3A";
-	P7 = "\x3B";
-	P8 = "\x3C";
-	P9 = "\x3D";
-	Pother = "\x3E";
+
+	Pai = [\uE002];
+	Pau = [\uE003];
+	Pkh = [\uE004];
+	Pgh = [\uE005];
+	Pch = [\uE006];
+	Pjh = [\uE007];
+	P_th = [\uE008];
+	P_dh = [\uE009];
+	Pth = [\uE00A];
+	Pdh = [\uE00B];
+	Pph = [\uE00C];
+	Pbh = [\uE00D];
+	Plongschwa = [\uE00E];
+
+	* { return 1, 0 }
 	// Ignore all diacritics (except for the distinctions ṛ/r ḷ/l).
 	// Treat aspirated and unaspirated as equivalent.
 	// Treat nasals as equivalent (this isn't great though).
-	Pa | Paa { return cursor, Pa }
-	Pi | Pii { return cursor, Pi }
-	Pu | Puu { return cursor, Pu }
-	P_r | P_rr { return cursor, Pṛ }
-	P_l | P_ll { return cursor, Pḷ }
-	Pe { return cursor, Pe }
+	"a" | "ā" { return cursor, 'a' }
+	"i" | "ī" { return cursor, 'i' }
+	"u" | "ū" { return cursor, 'u' }
+	"ṛ" | "ṝ" { return cursor, 'ṛ' }
+	"ḷ" | "ḹ" { return cursor, 'ḷ' }
+	"e" { return cursor, 'e' }
 	Pai { return cursor, Pai }
-	Po { return cursor, Po }
+	"o" { return cursor, 'o' }
 	Pau { return cursor, Pau }
-	P_h | Ph { return cursor, Ph }
-	Pk | Pkh | Pg | Pgh | Pk Pk | Pk Pkh | Pg Pg | Pg Pgh { return cursor, Pk }
-	P_m | Pf | Ppalataln | P_n | Pn | Pm | Pf Pf | Ppalataln Ppalataln | P_n P_n | Pn Pn | Pm Pm { return cursor, Pn }
-	Pc | Pch | Pj | Pjh | Pc Pc | Pc Pch | Pj Pj | Pj Pjh { return cursor, Pc }
-	P_t | P_th | P_d | P_dh | Pt | Pth | Pd | Pdh | P_t P_t | P_t P_th | P_d P_d | P_d P_dh | Pt Pt | Pt Pth | Pd Pd | Pd Pdh { return cursor, Pt }
-	Pp | Pph | Pb | Pbh | Pp Pp | Pp Pph | Pb Pb | Pb Pbh { return cursor, Pp }
-	Py | Py Py { return cursor, Py }
-	Pr | Pr Pr { return cursor, Pr }
-	Pl | Pl Pl { return cursor, Pl }
-	Pv | Pv Pv { return cursor, Pv }
-	Ppalatals | P_s | Ps { return cursor, Ps }
-	Pschwa | Plongschwa { return cursor, Pschwa }
-	Pother { return cursor, Pother }
+	"ḥ" | "h" { return cursor, 'h' }
+	"k" | Pkh | "g" | Pgh | "kk" | "k" Pkh | "gg" | "g" Pgh { return cursor, 'k' }
+	"ṃ" | "ṅ" | "ñ" | "ṇ" | "n" | "m" | "ṅṅ" | "ññ" | "ṇṇ" | "nn" | "mm" { return cursor, 'n' }
+	"c" | Pch | "j" | Pjh | "cc" | "c" Pch | "jj" | "j" Pjh { return cursor, 'c' }
+	"ṭ" | P_th | "ḍ" | P_dh | "t" | Pth | "d" | Pdh | "ṭṭ" | "ṭ" P_th | "ḍḍ" | "ḍ" P_dh | "tt" | "t" Pth | "dd" | "d" Pdh { return cursor, 't' }
+	"p" | Pph | "b" | Pbh | "pp" | "p" Pph | "bb" | "b" Pbh { return cursor, 'p' }
+	"y" | "yy" { return cursor, 'y' }
+	"r" | "rr" { return cursor, 'r' }
+	"l" | "ll" { return cursor, 'l' }
+	"v" | "vv" { return cursor, 'v' }
+	"ś" | "ṣ" | "s" { return cursor, 's' }
+	"ə" | Plongschwa { return cursor, 'ə' }
 	// Fallback to exactly one byte to avoid fatal panics.
-	[^] { return 1, int(encoded[0]) }
+	[^] {
+		r, size := utf8.DecodeRuneInString(encoded)
+		return size, r
+	}
 	*/
 	return 0, 0
 }
@@ -368,7 +231,7 @@ func encodeSequence(text string) string {
 			text = text[consumed:]
 			continue
 		}
-		seq.WriteByte(byte(rep))
+		seq.WriteRune(rep)
 		text = text[consumed:]
 	}
 	return seq.String()
@@ -388,8 +251,11 @@ func encodeSequenceWithBounds(text string) (string, []int) {
 			continue
 		}
 		endCursor := cursor + consumed
-		seq.WriteByte(byte(rep))
-		bounds = append(bounds, cursor, endCursor)
+		seq.WriteRune(rep)
+		n := utf8.RuneLen(rep)
+		for j := 0; j < n; j++ {
+			bounds = append(bounds, cursor, endCursor)
+		}
 		cursor = endCursor
 		text = text[consumed:]
 	}
@@ -402,10 +268,10 @@ func reduceSequence(encoded string) string {
 	var reduced strings.Builder
 	for len(encoded) > 0 {
 		consumed, rep := lexReduced(encoded)
-		if rep == Pnul {
+		if rep == 0 {
 			break
 		}
-		reduced.WriteByte(byte(rep))
+		reduced.WriteRune(rep)
 		encoded = encoded[consumed:]
 	}
 	return reduced.String()
@@ -419,19 +285,19 @@ func reduceSequenceWithBounds(encoded string) (string, []int) {
 	cursor := 0
 	for len(encoded) > 0 {
 		consumed, rep := lexReduced(encoded)
-		if rep == Pnul {
+		if rep == 0 {
 			break
 		}
-		reduced.WriteByte(byte(rep))
-		bounds = append(bounds, cursor, cursor+consumed)
+		reduced.WriteRune(rep)
+		n := utf8.RuneLen(rep)
+		for j := 0; j < n; j++ {
+			bounds = append(bounds, cursor, cursor+consumed)
+		}
 		cursor += consumed
 		encoded = encoded[consumed:]
 	}
 	return reduced.String(), bounds
 }
-
-const normalFirst = 0xE002
-const Slongschwa = string(rune(normalFirst + Plongschwa))
 
 var folder = cases.Fold()
 
@@ -460,11 +326,11 @@ func lexNormalPrefix(text string) (int, string, bool) {
 	"œ" | "Œ" { return cursor, "oe", false }
 	"đ" | "Đ" { return cursor, "d", false }
 	"r̥" | "R̥" { return cursor, "ṛ", false }
-	"r̥̄" | "R̥̄" { return cursor, "ṹ", false }
+	"r̥̄" | "R̥̄" { return cursor, "ṝ", false }
 	"l̥" | "L̥"{ return cursor, "ḷ", false }
 	"l̥̄" | "L̥̄" { return cursor, "ḹ", false }
 	"ә" | "Ә" { return cursor, "ə", false }
-	"ə̄" | "Ə̄" | "ә̄" | "Ә̄" { return cursor, Slongschwa, false }
+	"ə̄" | "Ə̄" | "ә̄" | "Ә̄" { return cursor, string(Plongschwa), false }
 	"ṁ" | "Ṁ" | "ṃ" | "Ṃ" | "m̐" | "M̐" | "m̃" | "M̃" { return cursor, "ṃ", false }
 	"ḥ" | "Ḥ" | "ḫ" | "Ḫ" | "ẖ" | "H̱" { return cursor, "ḥ", false }
 	// Fallback to Unicode case folding of the current full rune.
